2aeed7d3032dd6cd296afa8c03ae87d20e48ef76
Michalis Ioak <ioakimidismichalis@gmail.com>
2021-03-01 17:19:44 +0200

    null safety?

--- lib/src/speed_dial_child.dart
+++ lib/src/speed_dial_child.dart
@@ -3,28 +3,28 @@ import 'package:flutter/material.dart';
 /// Provides data for a speed dial child
 class SpeedDialChild {
   /// The key of the speed dial child.
-  final Key key;
+  final Key? key;
 
   /// The label to render to the left of the button
-  final String label;
+  final String? label;
 
   /// The style of the label
-  final TextStyle labelStyle;
+  final TextStyle? labelStyle;
 
   /// The background color of the label
-  final Color labelBackgroundColor;
+  final Color? labelBackgroundColor;
 
   /// If this is provided it will replace the default widget, therefore [label],
   /// [labelStyle] and [labelBackgroundColor] should be null
-  final Widget labelWidget;
+  final Widget? labelWidget;
 
-  final Widget child;
-  final Color backgroundColor;
-  final Color foregroundColor;
-  final double elevation;
-  final VoidCallback onTap;
-  final VoidCallback onLongPress;
-  final ShapeBorder shape;
+  final Widget? child;
+  final Color? backgroundColor;
+  final Color? foregroundColor;
+  final double? elevation;
+  final VoidCallback? onTap;
+  final VoidCallback? onLongPress;
+  final ShapeBorder? shape;
 
   SpeedDialChild({
     this.key,
