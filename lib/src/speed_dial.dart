3c70496d051afffa8b4a114d220a3d12617d9f5c
Prateek Sunal <prtksu@pm.me>
2022-10-12 10:08:30 +0530

    Update speed_dial.dart

--- lib/src/speed_dial.dart
+++ lib/src/speed_dial.dart
@@ -226,12 +226,11 @@ class _SpeedDialState extends State<SpeedDial>
   @override
   void dispose() {
     if (widget.renderOverlay && backgroundOverlay != null) {
-            if (_open) backgroundOverlay!.remove();
-            backgroundOverlay!.dispose();
-        }
+        if (_open && backgroundOverlay!.mounted) backgroundOverlay!.remove();
+        backgroundOverlay!.dispose();
     }
     if (overlayEntry != null) {
-        if (_open) overlayEntry!.remove();
+        if (_open && overlayEntry!.mounted) overlayEntry!.remove();
         overlayEntry!.dispose();
     }
     _controller.dispose();
